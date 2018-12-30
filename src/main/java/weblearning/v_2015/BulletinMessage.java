139e5613bb180bc6c9cf87d45177dba68a33edab
jjyyxx <obfishstudio@gmail.com>
2018-12-30 11:11:11 +0800

    remove duplicate

--- src/main/java/weblearning/v_2015/BulletinMessage.java
+++
@@ -1,7 +0,0 @@
-package weblearning.v_2015;
-
-@SuppressWarnings("unused")
-public class BulletinMessage {
-    public BaseMessage.CourseCourseware courseCourseware;
-    public BaseMessage.CourseOutlines courseOutlines;
-}
